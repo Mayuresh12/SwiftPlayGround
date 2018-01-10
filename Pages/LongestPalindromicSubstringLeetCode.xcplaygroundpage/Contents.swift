74e6cd110ee70bc92abbabef74b43382cd733532
Mayuresh Rao <mayureshvrao@gmail.com>
2018-01-09 23:55:28 -0800

    palidrome

--- Pages/LongestPalindromicSubstringLeetCode.xcplaygroundpage/Contents.swift
+++ Pages/LongestPalindromicSubstringLeetCode.xcplaygroundpage/Contents.swift
@@ -4,4 +4,20 @@ import Foundation
 
 var str = "Hello, playground"
 
-//: [Next](@next)
+
+func longestPalindrome(_ s: String) -> String {
+    var myArray = Array(s)
+    var  l = 0
+    var h = s.count - 1
+    while (h > l) {
+        if (myArray[l] != myArray[h]) {
+            print("Is not a palindrome = \(s)")
+            return ""
+        }
+        l = l + 1
+        h =  h - 1
+    }
+    print("Is Palindrome")
+    return s
+}
+print(longestPalindrome("a"))
