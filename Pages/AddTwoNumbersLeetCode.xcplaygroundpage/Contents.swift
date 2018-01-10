ec13795a720a76149f16b9e34850ae90a081efdd
Mayuresh Rao <mayureshvrao@gmail.com>
2018-01-09 22:22:38 -0800

    AddTwoNumber Leetcode Swift

--- Pages/AddTwoNumbersLeetCode.xcplaygroundpage/Contents.swift
+++ Pages/AddTwoNumbersLeetCode.xcplaygroundpage/Contents.swift
@@ -1,7 +1,12 @@
 //: [Previous](@previous)
 
-import Foundation
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+             self.val = val
+             self.next = nil
+     }
+}
 
-var str = "Hello, playground"
 
-//: [Next](@next)
