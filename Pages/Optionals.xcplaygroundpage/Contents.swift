b53d701d5b52153aaf34e506ee74022a4faa1e8d
Mayuresh Rao <mayureshvrao@gmail.com>
2018-03-25 21:47:06 -0700

    optionals

---
+++ Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var nickname: String?
+nickname = "Mayuresh"
+nickname = nil
+
+
+// exercise
+
+let myFavouriteSong = String?
+myFavouriteSong = "I am the best"
+myFavouriteSong = "My heart will go on"
+myFavouriteSong = nil
+
+let parsedInt = Int("Mayuresh") // the type of parsed Int is optional here because what if the string passed is not a number for eg  ***** parsedInt  = ("Mayuresh") which is  not a number will give nil hence it parsed Int is optional
+
+
